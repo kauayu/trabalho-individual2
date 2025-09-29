05e11833b5ea4dd798e9055d1d6b283fe406c036
kauayu <kaua.palmiro.ferreira@escola.pr.gov.br>
2025-09-29 10:00:22 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+document.getElementById("faq-btn").addEventListener("click", function() {
+    const faqAnswers = document.getElementById("faq-answers");
+    
+    if (faqAnswers.style.display === "none") {
+        faqAnswers.style.display = "block";
+    } else {
+        faqAnswers.style.display = "none";
+    }
+});
